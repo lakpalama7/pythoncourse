13b6b9ee29850c9b092401c68ee9acc3315cbd4d
lakpalama7 <sherpasvcet@gmail.com>
2025-10-01 18:04:42 +0300

    types of comments

---
+++ comment.py
@@ -0,0 +1,11 @@
+
+#this is first comment -- single line comment
+
+
+"""this is multi line comment
+this is multi line comment 
+with double quotations"""
+
+'''this is multi line commnet
+this is multi line commnet
+with single quotations'''
