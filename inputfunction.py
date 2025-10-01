654c45b2e4028029554e0b9d90c0a130d3056fd5
lakpalama7 <sherpasvcet@gmail.com>
2025-10-01 18:44:44 +0300

    Update inputfunction.py

--- inputfunction.py
+++ inputfunction.py
@@ -8,4 +8,13 @@ print("Your name is ",name, "and your age is ",age)
 first_num = int(input("Enter first number : "))
 second_num = int(input("Enter second number: "))
 number_sum = first_num + second_num
-print("the sum of",first_num, "and",second_num ,"is : ", number_sum)
+print("the sum of",first_num, "and",second_num ,"is : ", number_sum
+
+
+output:
+Enter your name:ram
+Enter your age: 34
+Your name is  ram and your age is  34
+Enter first number : 2
+Enter second number: 3
+the sum of 2 and 3 is :  5
