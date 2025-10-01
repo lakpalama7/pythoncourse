0644431ccda3505232e992e745636c6504e58805
lakpalama7 <sherpasvcet@gmail.com>
2025-10-01 18:21:55 +0300

    Update variable.py

--- variable.py
+++ variable.py
@@ -16,3 +16,15 @@ print("first number is ", first_num)
 first_num=3
 print("fist number is updated to", first_num)
 
+
+output:
+my name is ram
+ram is learning python
+ram wants to be python expert
+
+my name is sita
+sita is learning java
+sita wants to be java expert
+
+first number is  2
+fist number is updated to 3
