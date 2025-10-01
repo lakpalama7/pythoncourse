5bf167dea194ba4055cf377669b2f05959c4265e
lakpalama7 <sherpasvcet@gmail.com>
2025-10-01 18:20:48 +0300

    variable

---
+++ variable.py
@@ -0,0 +1,18 @@
+name="ram"
+language="python"
+print("my name is", name)
+print(name,"is learning",language)
+print(name,"wants to be",language,"expert")
+
+name="sita"
+language="java"
+print("my name is", name)
+print(name,"is learning",language)
+print(name,"wants to be",language,"expert")
+
+first_num=2
+print("first number is ", first_num)
+
+first_num=3
+print("fist number is updated to", first_num)
+
