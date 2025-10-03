bdd8f71554ea74d5478bf26a70730cc27ceaa0ff
lakpalama7 <sherpasvcet@gmail.com>
2025-10-03 20:37:58 +0300

    python bool functions

---
+++ pythonboolfunction.py
@@ -0,0 +1,26 @@
+name=""
+email="ram@gmail.com"
+phone="1235-1256"
+
+#allow registration if any field is provided
+print(any([name,email,phone]))
+
+name="ram"
+email="ram@gmail.com"
+phone="1235-1256"
+#allow registration only if all field is provided
+
+print(all([name,email,phone]))
+
+print("--"*10)
+
+print(isinstance(123,int))
+print(isinstance(12.3, float))
+print(isinstance("abc",str))
+print(isinstance(True,bool))
+
+print("="*10)
+
+print("Hello".endswith("o"))
+print("Hello".startswith("H"))
+print("Hello".startswith("h"))
