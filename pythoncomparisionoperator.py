98cd8048261534dfe80622df1fddc622b01f8096
lakpalama7 <sherpasvcet@gmail.com>
2025-10-03 20:54:24 +0300

    Python Comparision Operators

---
+++ pythoncomparisionoperator.py
@@ -0,0 +1,18 @@
+print(10 == 10)
+print(3<=7)
+print(7<2)
+print(10 != 10)
+
+print("--"*10)
+print("a" < "c")
+print("a" == "A")
+print("a" != "A")
+
+print("-"*10)
+
+print(1<4<6)
+print(3<2<1)
+print(3>2<3)
+
+age=20
+print(18 <= age <=30)
